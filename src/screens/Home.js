7e1364bdff97bbe5eed4101d7ec3075f5a42dc63
icortez <icortez@ufg.edu.sv>
2021-09-10 11:40:39 -0600

    add loading and carlist to flatlist and also getting state and dispatch action to get data when app loads first time

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import {FlatList, Text} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
 
-export const Home = () => (
-  <View>
-    <Text>home</Text>
-  </View>
-);
+import {Loading} from '../components/Loading';
+import {CardList} from '../components/CardList';
+
+import * as actions from '../redux/actions/VendingMachineActions';
+
+export const Home = () => {
+  const dispatch = useDispatch();
+  const {data, loading} = useSelector(state => state.VendingMachineReducer);
+
+  useEffect(() => {
+    dispatch(actions.getDataVendingMachine());
+  }, []);
+
+  const renderItem = ({item}) => (
+    <CardList
+      name={item.name}
+      image={item.thumbnail}
+      time={item.preparation_time}
+    />
+  );
+  const keyExtractor = item => item.id;
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={keyExtractor}
+        />
+      )}
+    </>
+  );
+};
